24f4ead4f1fcf1d92ec13b2972857e2636ad121a
Ramadan <futurezorro@gmail.com>
2023-06-14 20:15:08 +0300

    Ramadan

--- src/components/man&weomen/MW_Section.jsx
+++ src/components/man&weomen/MW_Section.jsx
@@ -4,13 +4,7 @@ const MW_Section = (props) => {
   return (
     <div className=" relative flex  font-serif  justify-evenly items-center group  overflow-hidden rounded-b-lg shadow-md cursor-pointer w-[370px] max-h-[250px] ">
       <div className=" group-hover:scale-[1.1] duration-500">
-        <Image
-          src={props.src_image}
-          alt={props.alt}
-          width={370}
-          height={250}
-          placeholder="blure"
-        />
+        <Image src={props.src_image} alt={props.alt} width={370} height={250} />
       </div>
       <div className=" absolute w-full font-medium h-[60px] group-hover:h-full group-hover:opacity-60 text-center bg-black opacity-80 flex flex-col justify-center items-center duration-200  bottom-0 ">
         <h3 className=" text-white max-sm:text-[.9rem] text-[1rem] tracking-wide ">
